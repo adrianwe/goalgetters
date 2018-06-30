1a5d6abed90bf1f482d5f0715e637a1b21b15749
adrianwe <adrian.wenke@gmail.com>
2018-06-30 11:02:36 +0200

    new 404 v2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import styles from './impressum.module.css'
+import styles from './404.module.css'
 
 const NotFoundPage = () => (
   <div>
     <div className={styles.navBackground}></div>
-    <div className={styles.impressum}>
-      <div className={styles.impressumTitle}>404 Not Found</div>
+    <div className={styles.box404}>
+      <div className={styles.title404}>404: Not Found</div>
       <div>Diese Seite existiert nicht.</div>
     </div>
   </div>
