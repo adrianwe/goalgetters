6fec42cdb1fffe0bfd201f20c77c9a296e40f73c
adrianwe <adrian.wenke@gmail.com>
2018-07-04 10:39:17 +0200

    fixed delete tags feature on grid

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 exports.shouldUpdateScroll = ({ prevRouterProps, pathname }) => {
   if (prevRouterProps && pathname) {
-    if ((prevRouterProps.location.pathname.substring(0, 4) === '/tag' || prevRouterProps.location.pathname.substring(0, 12) === '/inspiration') && pathname.substring(0, 4) === '/tag') {
+    if ((prevRouterProps.location.pathname.substring(0, 4) === '/tag' || prevRouterProps.location.pathname.substring(0, 12) === '/inspiration') && (pathname.substring(0, 4) === '/tag' || pathname.substring(0, 12) === '/inspiration')) {
       return false
     }
   }
